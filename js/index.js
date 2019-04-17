3c1813852799dd232d6a85e3db756516e12b0f0e
digitaladvertisingteam <47647996+digitaladvertisingteam@users.noreply.github.com>
2019-04-17 12:53:32 -0500

    Subiendo carpeta Js e index

--- js/index.js
+++ js/index.js
@@ -1,45 +1,11 @@
-$(document).scroll(function() {
-    var scroll = $(this).scrollTop();
-    if (scroll >= 150) {
-      $("#popUp").css("margin-left", "-425px");
-      $("#plus").css("margin-left", "0px");
-    }
-  });
-  
-  $("#plus").click(function() {
-    $("#popUp").css("margin-left", "0px");
-    $("#plus").css("margin-left", "-425px");
-  });
-  
-  $("#close").click(function() {
-    $("#popUp").css("margin-left", "-425px");
-    $("#plus").css("margin-left", "0px");
-  });
-
-  var module = angular.module('numberApp', []);
-
-module.controller('NumberController', function($scope) {
-  $scope.getInput = function() {
-    if($scope.min > $scope.max) {
-      //Swap variables
-      [$scope.min,$scope.max] = [$scope.max,$scope.min]; 
-    }
-    $scope.generateNumber();
-  }
-  
-  $scope.loadNum = function() {
-    $scope.min = 1;
-    $scope.max = 10;
-    $scope.generateNumber();
-  }
-
-  $scope.generateNumber = function() {
-    $scope.randomNumber = Math.floor(Math.random()*($scope.max-$scope.min+1)+$scope.min);
-  }
-});
-
-  $(".hover").mouseleave(
-    function() {
-      $(this).removeClass("hover");
-    }
-  );
+   //TESTIMONIAL SLIDER
+    if ($(".ts-testimonial-slide").length > 0) {
+    
+        $(".ts-testimonial-slide").owlCarousel({
+            autoPlay: 4000,
+            slideSpeed: 1000,
+            navigation: false,
+            pagination: true,
+            singleItem: true
+        });
+    };
